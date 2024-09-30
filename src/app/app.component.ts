b8b427ffbcbc247a4230535bedd1da182879f0e6
Przemyslaw Koziel <mos.koziel@gmail.com>
2024-09-30 16:12:35 +0200

    feat: eslint added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 @Component({
-  selector: 'app-root',
+  selector: 'myo-root',
   standalone: true,
   imports: [RouterOutlet, HlmButtonDirective],
   templateUrl: './app.component.html',
